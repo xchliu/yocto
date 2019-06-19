cc4aa694de26435f2f185ab536d0c394405dffce
makang <makang@hotpu.cn>
2019-06-19 21:29:27 +0800

    add sql parser demo first commit

--- yoctodb/src/server/parser.go
+++ yoctodb/src/server/parser.go
@@ -1,1 +1,33 @@
 package main
+
+import (
+	"fmt"
+	"github.com/antlr/antlr4/runtime/Go/antlr"
+	"yocto/yoctodb/src/server/grammer/parser"
+)
+
+type TreeShapeListener struct {
+	*parser.BaseMySqlParserListener
+}
+
+func NewTreeShapeListener() *TreeShapeListener {
+	return new(TreeShapeListener)
+}
+
+func (this *TreeShapeListener) EnterEveryRule(ctx antlr.ParserRuleContext) {
+	fmt.Println(ctx.GetText())
+}
+
+func main() {
+	//input, _ := antlr.NewFileStream(os.Args[1])
+	input := antlr.NewInputStream("SELECT NAME FROM AT WHERE NAME LIKE 'makang%'")
+	lexer := parser.NewMySqlLexer(input)
+	stream := antlr.NewCommonTokenStream(lexer, 0)
+	p := parser.NewMySqlParser(stream)
+	p.AddErrorListener(antlr.NewDiagnosticErrorListener(true))
+	p.BuildParseTrees = true
+	tree := p.Root()
+	//antlr.ParseTreeWalkerDefault.Walk()
+	//antlr.ParseTreeVisitor(tree)
+	fmt.Println(tree.ToStringTree(p.GetTokenNames(), p.BaseParser))
+}
