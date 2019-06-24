82941a0f5ea69183659a4b1bcc76872020d9dfd1
makang <makang@hotpu.cn>
2019-06-24 23:42:32 +0800

    DDL define and get demo

--- yoctodb/src/parser/parser.go
+++ yoctodb/src/parser/parser.go
@@ -1,10 +1,9 @@
-package parser
+package main
 
 import (
 	"fmt"
-	"yoctodb/src/server/grammer/parser"
-
 	"github.com/antlr/antlr4/runtime/Go/antlr"
+	"yocto/yoctodb/src/parser/grammer/parser"
 )
 
 type TreeShapeListener struct {
@@ -21,14 +20,16 @@ func (this *TreeShapeListener) EnterEveryRule(ctx antlr.ParserRuleContext) {
 
 func main() {
 	//input, _ := antlr.NewFileStream(os.Args[1])
-	input := antlr.NewInputStream("SELECT NAME FROM AT WHERE NAME LIKE 'makang%'")
+	//input := antlr.NewInputStream("SELECT NAME FROM AT WHERE NAME LIKE 'makang%'")
+	input := antlr.NewInputStream("CREATE TABLE 'makang' (C1 INT, C2 VARCHAR(120))")
 	lexer := parser.NewMySqlLexer(input)
 	stream := antlr.NewCommonTokenStream(lexer, 0)
 	p := parser.NewMySqlParser(stream)
 	p.AddErrorListener(antlr.NewDiagnosticErrorListener(true))
 	p.BuildParseTrees = true
-	tree := p.Root()
+	tree := p.DdlStatement()
 	//antlr.ParseTreeWalkerDefault.Walk()
 	//antlr.ParseTreeVisitor(tree)
+	//fmt.Println(tree.get)
 	fmt.Println(tree.ToStringTree(p.GetTokenNames(), p.BaseParser))
 }
