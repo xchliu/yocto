2b5a678a19d081182665e0eba06a0f7b6c7ac90e
makang <makang@hotpu.cn>
2019-06-28 22:48:35 +0800

    modify parser_api, 2bcontinued

--- src/parser/parser.go
+++ src/parser/parser.go
@@ -3,33 +3,59 @@ package main
 import (
 	"fmt"
 	"github.com/antlr/antlr4/runtime/Go/antlr"
-	"yocto/yoctodb/src/parser/grammer/parser"
+	"yocto/src/parser/grammer/parser"
 )
 
-type TreeShapeListener struct {
+type SQLObject struct {
+	DB         string
+	SQLQuery   string
+	SQLType    int
+	SQLCommand int
+	*YoctoSQLBaseListener
+}
+
+type YoctoSQLBaseListener struct {
 	*parser.BaseMySqlParserListener
 }
 
-func NewTreeShapeListener() *TreeShapeListener {
-	return new(TreeShapeListener)
+func YoctoSQLListener() *YoctoSQLBaseListener {
+	//a := new(SQLObject)
+	return new(YoctoSQLBaseListener)
 }
 
-func (this *TreeShapeListener) EnterEveryRule(ctx antlr.ParserRuleContext) {
-	fmt.Println(ctx.GetText())
+type YYoctoSQLListener struct {
+	antlr.ParseTreeListener
+}
+
+func Build_paser(query, db string) (p *parser.MySqlParser) {
+	input := antlr.NewInputStream(query)
+	lexer := parser.NewMySqlLexer(input)
+	stream := antlr.NewCommonTokenStream(lexer, 0)
+	p = parser.NewMySqlParser(stream)
+	//p.AddErrorListener(antlr.NewDiagnosticErrorListener(true))
+	p.BuildParseTrees = true
+	return p
+}
+
+func (this *SQLObject) EnterDdlStatement(ctx *parser.DdlStatementContext) {
+	this.SQLType = parser.MySqlParserRULE_ddlStatement
+}
+
+func (this *SQLObject) EnterDmlStatement(ctx *parser.DmlStatementContext) {
+	this.SQLType = parser.MySqlParserRULE_dmlStatement
+}
+
+func test(q string) {
+	s := new(SQLObject)
+	s.SQLQuery = q
+	p := Build_paser(s.SQLQuery, "")
+	tree := p.Root()
+	antlr.ParseTreeWalkerDefault.Walk(YoctoSQLListener(), tree)
+	//fmt.Println(sql_obj.pTree.ToStringTree(sql_obj.p.GetTokenNames(), sql_obj.p.BaseParser))
+	fmt.Println(s.SQLType)
 }
 
 func main() {
-	//input, _ := antlr.NewFileStream(os.Args[1])
-	//input := antlr.NewInputStream("SELECT NAME FROM AT WHERE NAME LIKE 'makang%'")
-	input := antlr.NewInputStream("CREATE TABLE 'makang' (C1 INT, C2 VARCHAR(120))")
-	lexer := parser.NewMySqlLexer(input)
-	stream := antlr.NewCommonTokenStream(lexer, 0)
-	p := parser.NewMySqlParser(stream)
-	p.AddErrorListener(antlr.NewDiagnosticErrorListener(true))
-	p.BuildParseTrees = true
-	tree := p.DdlStatement()
-	//antlr.ParseTreeWalkerDefault.Walk()
-	//antlr.ParseTreeVisitor(tree)
-	//fmt.Println(tree.get)
-	fmt.Println(tree.ToStringTree(p.GetTokenNames(), p.BaseParser))
+	test("CREATE TABLE T1(C1 INT)")
+	fmt.Println()
 }
